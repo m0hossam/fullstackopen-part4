f93980f5f70764e54d9800def75d35fea8b64919
m0hossam <mohossam1326@gmail.com>
2024-10-17 21:34:23 +0300

    Finish exercise 4.2

---
+++ controllers/blogs.js
@@ -0,0 +1,17 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (_request, response) => {
+Blog.find({}).then(blogs => {
+    response.json(blogs)
+    })
+})
+
+blogsRouter.post('/', (request, response) => {
+const blog = new Blog(request.body)
+blog.save().then(result => {
+    response.status(201).json(result)
+    })
+})
+
+module.exports = blogsRouter
